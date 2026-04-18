96df1a6dac478f617e67b78515771488cdcc5cb6
Bruno spanier <brunospanier@hotmail.com>
2026-04-18 18:04:17 -0300

    Ajuste de rotas e controllers com extensao js

--- src/api/routes/productRoutes.js
+++ src/api/routes/productRoutes.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// O ERRO ESTAVA AQUI: Você estava chamando o Repository em vez do Controller
-// Certifique-se de que o nome do arquivo na pasta controllers é productController.js
+// Caminho corrigido: volta um nível para 'api' e entra em 'controllers'
 const productController = require('../controllers/productController');
 
-// Rota para listar todos os produtos
 router.get('/', productController.findAll);
-
-// Rota para cadastrar um novo produto
 router.post('/', productController.create);
-
-// Rota para excluir um produto pelo ID
 router.delete('/:id', productController.delete);
 
 module.exports = router;
