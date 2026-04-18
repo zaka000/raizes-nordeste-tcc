b1129dc9a0ed5ce64b655016c6e583bab688276a
Bruno spanier <brunospanier@hotmail.com>
2026-04-18 16:47:41 -0300

    Fixex respository

--- src/infrastructure/repositories/productRepository.js
+++ src/infrastructure/repositories/productRepository.js
@@ -1,30 +1,33 @@
-// Adicionamos as chaves { pool } para pegar apenas a conexão do banco
+
 const { pool } = require('../../config/db');
 
 const productRepository = {
+   
     create: async (nome, preco, categoria) => {
         try {
-            // Trocamos 'db.query' por 'pool.query'
             const [result] = await pool.query(
-                'INSERT INTO produtos (nome, preco, categoria) VALUES (?, ?, ?)', 
+                'INSERT INTO produtos (nome, preco, categoria) VALUES (?, ?, ?)',
                 [nome, preco, categoria]
             );
             return result;
         } catch (error) {
-            console.error("Erro ao inserir produto:", error.message);
-            throw error; // Lança o erro para o controller tratar
+            console.error("🚨 Erro no SQL de Produto (Create):", error.message);
+            throw error; 
         }
     },
+    
+    
     findAll: async () => {
         try {
-            // Trocamos 'db.query' por 'pool.query'
-            const [rows] = await pool.query('SELECT * FROM produtos');
+            
+            const [rows] = await pool.query('SELECT * FROM produtos ORDER BY id DESC');
             return rows;
         } catch (error) {
-            console.error("Erro ao buscar produtos:", error.message);
-            return []; // Retorna lista vazia para não quebrar o frontend
+            console.error("🚨 Erro no SQL de Produto (FindAll):", error.message);
+            return []; 
         }
     }
 };
+
 
 module.exports = productRepository;
