9478a9809ca66b31ee13ce0ece95a06fa45ec93d
Bruno spanier <brunospanier@hotmail.com>
2026-04-18 18:41:36 -0300

    Reinserindo arquivo com nome corrigido

---
+++ src/api/controllers/productRepository.js
@@ -0,0 +1,37 @@
+// Importando o repository com a extensão .js
+const productRepository = require('../repositories/productRepository');
+
+const productController = {
+    findAll: async (req, res) => {
+        try {
+            const products = await productRepository.findAll();
+            res.json(products);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+    create: async (req, res) => {
+        try {
+            const { nome, preco, categoria } = req.body;
+            await productRepository.create(nome, preco, categoria);
+            res.status(201).json({ message: "Produto criado!" });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+    delete: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const result = await productRepository.delete(id);
+            if (result.affectedRows > 0) {
+                res.json({ message: "Excluído com sucesso" });
+            } else {
+                res.status(404).json({ message: "Não encontrado" });
+            }
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+};
+
+module.exports = productController;
